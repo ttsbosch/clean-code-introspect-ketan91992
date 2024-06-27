eca92b357ce4034a242a2cd556f48e0aaf305339
ketan91992 <ketan.tarkas9@gmail.com>
2024-06-27 15:05:55 +0530

    Update StringCalculator.h

--- StringCalculator.h
+++ StringCalculator.h
@@ -5,4 +5,6 @@ using namespace std;
 class StringCalculator{
     public:
     int add(string input);
+    std::vector<std::string> split(const std::string& str, const std::string& delimiter);
+    int sum(const std::vector<std::string>& numbers);
 };
