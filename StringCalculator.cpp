a675885a4dc47bf07b81276476f87d8818f1d2c4
ketan91992 <ketan.tarkas9@gmail.com>
2024-06-27 15:01:20 +0530

    Update StringCalculator.cpp

--- StringCalculator.cpp
+++ StringCalculator.cpp
@@ -1,5 +1,22 @@
 #include "StringCalculator.h"
 
 int StringCalculator::add(string input){
-        return 0;
+        if (input.empty()) return 0;
+
+        std::string delimiter = ",";
+        std::string numString = input;
+
+        // Handle custom delimiter
+        if (input.substr(0, 2) == "//") {
+            size_t newlinePos = input.find("\n");
+            delimiter = input.substr(2, newlinePos - 2);
+            numString = input.substr(newlinePos + 1);
+        }
+
+        // Replace newlines with the delimiter
+        std::replace(numString.begin(), numString.end(), '\n', delimiter[0]);
+
+        // Split the numbers by the delimiter
+        std::vector<std::string> tokens = split(numString, delimiter);
+        return sum(tokens);
 }
